4326e8bbda8d19a3b2bc99064941551e3382cb20
Pritesh <bhuravanepritesh@gmail.com>
2025-05-23 13:24:14 +0530

    HotelForm added

--- client/src/componenets/Hero.jsx
+++ client/src/componenets/Hero.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import HotelForm from "./HotelForm";
 
 const Hero = () => {
   return (
     <div className='flex flex-col items-start justify-center h-screen bg-cover bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-center px-6 md:px-16 lg:px-24 xl:px-32 text-white'>
+      <p className=" bg-[#49B9FF]/50 px-3.5 py-1 rounded-full  mt-20">
+        The Ultimate Hotel Experience
+      </p>
+      <h1 className=" font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4 ">
+        Discover Your Perfect Gateway Destination
+      </h1>
+      <p className=" max-w-130 mt-2 text-sm md:text-base">
+        Unparalleled luxury and comfort await at the world's most exclusive
+        hotels and resorts. Start your journey today
+      </p>
 
+      <HotelForm/>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
