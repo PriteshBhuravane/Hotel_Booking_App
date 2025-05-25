46ae083de45ef5606c03469a3b00482ea1a32228
Pritesh <bhuravanepritesh@gmail.com>
2025-05-25 13:32:36 +0530

    frontend home page is done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Hero from '../componenets/Hero'
 import FeatureDestination from '../componenets/FeatureDestination'
+import ExclusiveOffers from '../componenets/ExclusiveOffers'
+import Testimonial from '../componenets/Testimonial'
+import NewsLetter from '../componenets/NewsLetter'
+import Footer from '../componenets/Footer'
 
 const Home = () => {
   return (
     <>
         <Hero/>
         <FeatureDestination/>
+        <ExclusiveOffers/>
+        <Testimonial/>
+        <NewsLetter/>
     </>
   )
 }
